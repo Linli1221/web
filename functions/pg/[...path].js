c44c390ec3e7983a511327a11bd4c59f3aeb22a6
linzero <root@linli.eu.org>
2025-07-18 18:42:14 +0800

    修复 API 路径，移除多余的路径前缀，并更新构建脚本以复制函数到 dist 目录

--- functions/pg/[...path].js
+++ functions/pg/[...path].js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const path = params.path.join('/');
   
-  const targetUrl = `https://api.352287.xyz/pg/${path}${url.search}`;
+  const targetUrl = `https://api.352287.xyz/${path}${url.search}`;
 
   const response = await fetch(targetUrl, {
     method: request.method,
